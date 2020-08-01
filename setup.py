979f58265c342fc174b37d71f35f7150166c6b97
zhaofeng-shu33 <616545598@qq.com>
2020-08-01 14:47:13 +0800

    add package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name="Word_edit",
+    version="0.1",
+    author="zhaofeng-shu33",
+    description="backend for xinyu",
+    url="https://github.com/zhaofeng-shu33/DeutschLernen_server",
+    author_email="616545598@qq.com",
+    install_requires=['lxml', 'beautifulsoup4'],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Programming Language :: Python :: 3"
+    ]
+)
