60fb0b55806925f35daf655e4f171083c424af8a
zhaofeng-shu33 <616545598@qq.com>
2020-08-01 16:46:09 +0800

    requestcontext

--- Word_edit/views.py
+++ Word_edit/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.template import RequestContext
 from .html_form_to_xml import parsegen, savedit, addWord
 from django.template import loader
 
@@ -7,7 +8,7 @@ def create_response(request):
     """
     response for "Word_edit/create_new_word",GET to create new word, POST to update old word
     """
-    context={}
+    
     rq = request.POST
     if rq:
         reqsheet, err, err_str = parsegen(rq)
@@ -16,8 +17,8 @@ def create_response(request):
         savedit(reqsheet)
         return HttpResponse('All right, click <a href="http://'+request.get_host()+request.get_full_path()+'">create another</a>')
     else:
-        # GET Method here
-        context['wordAddrchoice'] = 'None'
-        context['isCreated'] = 'True'
+        # GET METHOD
+        context = RequestContext(request, {'wordAddrchoice': 'None',
+            'isCreated': 'True'})
     template = loader.get_template('Word_edit/editing_interface.html')
     return HttpResponse(template.render(context))
